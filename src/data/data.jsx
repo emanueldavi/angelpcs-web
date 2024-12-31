068402fce3568aaf375d368a71e7b409f6d97942
Emanuel <126615311+emanueldavi@users.noreply.github.com>
2024-12-31 02:32:53 -0400

    add Product section

---
+++ src/data/data.jsx
@@ -0,0 +1,34 @@
+export const LAPTOPS_DATA = [
+    {
+        name: 'Lenovo T470S',
+        processador:'Intel i5 de 7ma',
+        ram: '8GB',
+        disk: '256GB',
+        screen: '14"',
+        img: '/laptops/lenovo-t470s.jpg'
+    },
+    {
+        name: 'Lenovo T470S',
+        processador:'Intel i5 de 7ma',
+        ram: '8GB',
+        disk: '256GB',
+        screen: '14"',
+        img: '/laptops/lenovo-t470s.jpg'
+    },
+    {
+        name: 'Lenovo T470S',
+        processador:'Intel i5 de 7ma',
+        ram: '8GB',
+        disk: '256GB',
+        screen: '14"',
+        img: '/laptops/lenovo-t470s.jpg'
+    },
+    {
+        name: 'Lenovo T470S',
+        processador:'Intel i5 de 7ma',
+        ram: '8GB',
+        disk: '256GB',
+        screen: '14"',
+        img: '/laptops/lenovo-t470s.jpg'
+    },
+]
